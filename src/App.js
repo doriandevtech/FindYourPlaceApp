1c589d8ae44b9bc80bf99f05a83a06144194a104
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-02-21 11:35:40 +0100

    adding App display

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from "./api";
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+
+    const [places, setPlaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({ lat: 52.520007, lng: 13.404954});
+    const [bounds, setBounds] = useState(
+        // '11.847676',
+        // '12.838442',
+        // '109.095887',
+        // '109.149359',
+        null
+    );
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        })
+    }, []);
+
+    useEffect(() => {
+      getPlacesData(bounds)
+        .then((data) => {
+            console.log(data);
+
+            setPlaces(data);
+        })
+    }, [coordinates, bounds])
+    
+    
+
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    <List places={places} />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default App;
