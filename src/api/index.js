ebc9bec62c219f275720059572c192e89f5b9854
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-02-24 18:17:45 +0100

    create: API call to get restaurants data

---
+++ src/api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(URL, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY
+          }
+        });
+
+        return data;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
