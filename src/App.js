a24a14cff5fdf1678b913521d36950aedd7f0bf1
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-06-08 22:00:56 +0200

    add prettier formating settings

--- src/App.js
+++ src/App.js
@@ -1,60 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { CssBaseline, Grid } from '@material-ui/core';
+import { CssBaseline, Grid } from "@material-ui/core";
 
 import { getPlacesData } from "./api";
 
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Map from './components/Map/Map';
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
-    const [places, setPlaces] = useState([]);
-    const [childClicked, setChildClicked] = useState(null);
+  const [places, setPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState(null);
 
-    const [coordinates, setCoordinates] = useState({});
-    const [bounds, setBounds] = useState({});
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
 
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
-            setCoordinates({ lat: latitude, lng: longitude });
-        })
-    }, []);
-
-    useEffect(() => {
-        setIsLoading(true);
-      getPlacesData(bounds.sw, bounds.ne)
-        .then((data) => {
-            setPlaces(data);
-            setIsLoading(false);
-        })
-    }, [coordinates, bounds])
-
-    return (
-        <>
-            <CssBaseline />
-            <Header />
-            <Grid container spacing={3} style={{ width: '100%' }}>
-                <Grid item xs={12} md={4}>
-                    <List
-                        places={places}
-                        childClicked={childClicked}
-                        isLoading={isLoading}
-                    />
-                </Grid>
-                <Grid item xs={12} md={8}>
-                    <Map 
-                        setCoordinates={setCoordinates}
-                        setBounds={setBounds}
-                        coordinates={coordinates}
-                        places={places}
-                        setChildClicked={setChildClicked}
-                    />
-                </Grid>
-            </Grid>
-        </>
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
     );
-}
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
+      setPlaces(data);
+      setIsLoading(false);
+    });
+  }, [coordinates, bounds]);
+
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default App;
